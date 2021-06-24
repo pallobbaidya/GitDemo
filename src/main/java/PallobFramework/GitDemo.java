cea51185d3c3c2b47e93e8432ae2ab45bff7a74c
pallobbaidya <pallobbaidya@gmail.com>
2021-06-24 13:25:22 +0530

    First Commit

---
+++ src/main/java/PallobFramework/GitDemo.java
@@ -0,0 +1,12 @@
+
+
+
+package PallobFramework;
+
+public class GitDemo {
+
+
+
+
+
+}
---
+++ src/test/java/PallobFramework/AppTest.java
@@ -0,0 +1,22 @@
+package PallobFramework;
+
+
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void GitFirstTest()
+    {
+
+      System.out.println("This is the first line");
+    }
+}
